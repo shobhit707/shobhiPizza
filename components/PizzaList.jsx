a71b9ff120feefbec37596f417f4d1ca1b31b730
shobhit707 <50417428+shobhit707@users.noreply.github.com>
2022-01-13 10:46:26 +0530

    Update PizzaList.jsx

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -7,9 +7,7 @@ const PizzaList = ({pizzaList}) => {
     <div className={styles.container}>
       <div className={styles.desc}>
       <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
-       What’s better than having a crispy melty pizza, you ask?
-Having that crispy and melty pizza in the comfort of your own home with the ones you love, we say.
-With ShobhiPizza it is always “Rishton ka time”. Whether it's a treat for your promotion, a kid topping his class or winning the heart of your wife who is too tired to cook after a long day at work! A cheesy slice of the best pizza is all one needs to put things into perspective and start any celebration. Plus, you do not even need to rush to the restaurant to have one now. A call, a few clicks on our website or a few touches on the mobile screen is all you have to do to have that tempting, light-on-the-pocket pizza at your doorstep.
+       <p>{"What’s better than having a crispy melty pizza, you ask? Having that crispy and melty pizza in the comfort of your own home with the ones you love, we say. With ShobhiPizza it is always “Rishton ka time”. Whether it's a treat for your promotion, a kid topping his class or winning the heart of your wife who is too tired to cook after a long day at work! A cheesy slice of the best pizza is all one needs to put things into perspective and start any celebration. Plus, you do not even need to rush to the restaurant to have one now. A call, a few clicks on our website or a few touches on the mobile screen is all you have to do to have that tempting, light-on-the-pocket pizza at your doorstep."}</p>
 
       </div>
       <section id="menu">
