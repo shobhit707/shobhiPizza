abbba76c86fe999d0eb60b853a0d48bb3ec6e7fd
shobhit707 <50417428+shobhit707@users.noreply.github.com>
2022-01-13 13:00:45 +0530

    Update Cart.jsx

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -27,7 +27,7 @@ const Cart = () => {
 
   const createOrder = async (data) => {
     try {
-      const res = await axios.post("http://localhost:3000/api/orders", data);
+      const res = await axios.post("https://shobhi-pizza-gulivpi0h-shobhitbhadoriya123-gmailcom.vercel.app/api/orders", data);
       if (res.status === 201) {
         dispatch(reset());
         toast.success("Order Placed Succesfully");
