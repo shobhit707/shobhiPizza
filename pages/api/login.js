f58598858537f4b9d54a991f5a2a50f9bf870c2f
shobhit707 <50417428+shobhit707@users.noreply.github.com>
2022-01-13 13:09:08 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,8 +4,8 @@ const handler = (req, res) => {
   if (req.method === "POST") {
     const { username, password } = req.body;
     if (
-      username === process.env.ADMIN_USERNAME &&
-      password === process.env.ADMIN_PASSWORD
+      username === "shobhi" &&
+      password === "6769"
     ) {
       res.setHeader(
         "Set-Cookie",
